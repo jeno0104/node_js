fcb04d474ea159393338ce12e8af75983905dd69
zenoo97 <jaehun4086@naver.com>
2024-06-19 15:06:11 +0900

    [Update] 로그인 기능 완성
    1. bcrypt
    2. jwt 토큰

--- New/myContacts/app.js
+++ New/myContacts/app.js
@@ -3,18 +3,18 @@ const app = express();
 
 const dbConnect = require("./config/dbConnect");
 const methodOverride = require("method-override");
-app.get("/", (req, res) => {
-  res.send("hello node");
-});
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
 app.use(express.static("./public"));
 app.use(methodOverride("_method"));
+
 dbConnect();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/", require("./routes/loginRoutes"));
 app.use("/contacts", require("./routes/contactRoutes"));
 
 app.listen(3000, () => {
---
+++ New/myContacts/controllers/loginController.js
@@ -0,0 +1,54 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
+require("dotenv").config();
+// Get login page
+// Get /
+const getLogin = (req, res) => {
+  res.render("home");
+};
+const jwtSecret = process.env.JWT_SECRET;
+const jwt = require("jsonwebtoken");
+// Login user
+// POST /
+const loginUser = asyncHandler(async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res.json({ message: "일치하는 사용자가 없습니다." });
+  }
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    return res.json({ message: "비밀번호가 맞지 않습니다." });
+  }
+  const token = jwt.sign({ id: user._id }, jwtSecret);
+  res.cookie("token", token, { httpOnly: true }); // 쿠키를 만드는 함수
+  res.redirect("/contacts");
+});
+
+// Register page
+// GET /register
+
+const getRegister = (req, res) => {
+  res.render("register");
+};
+
+const Register = (req, res) => {
+  const { id, password } = req.body;
+};
+
+// Register user
+// POST /register
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { username, password, password2 } = req.body;
+
+  if (password === password2) {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({ username, password: hashedPassword });
+    res.json({ message: "Register successful", user });
+  } else {
+    res.send("Register Failed");
+  }
+});
+module.exports = { getLogin, loginUser, getRegister, registerUser };
